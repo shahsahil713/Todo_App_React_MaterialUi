895104cf856f53f8a71b49cc4f86b3691367c445
Shah Sahil <shahsahil713@gmail.com>
2020-06-14 13:03:10 +0530

    Updates

---
+++ precache-manifest.670a9d2e5122feba36f8216a790a0682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675c00cce89773d0060a54c6df9aff8e",
+    "url": "/Todo_App_React_MaterialUi/index.html"
+  },
+  {
+    "revision": "64e69763d1c223bb65a3",
+    "url": "/Todo_App_React_MaterialUi/static/css/main.ff41b3c8.chunk.css"
+  },
+  {
+    "revision": "7d4e6585674648daf61b",
+    "url": "/Todo_App_React_MaterialUi/static/js/2.289cd482.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo_App_React_MaterialUi/static/js/2.289cd482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e69763d1c223bb65a3",
+    "url": "/Todo_App_React_MaterialUi/static/js/main.9cfee74b.chunk.js"
+  },
+  {
+    "revision": "4ac7402a979dc74d8cf0",
+    "url": "/Todo_App_React_MaterialUi/static/js/runtime-main.8b155e12.js"
+  }
+]);
